15abf2473bb61090a1b3be6ff190f2f92ef69c2a
Luis Fernando Venegas Avila <53911844+Crowexe@users.noreply.github.com>
2022-06-01 03:03:53 -0500

    Home and Login
    Estructura básica de pagina iniciada

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+const Home = () => {
+
+	return (
+		<>
+			<h1 id="title">Fly me to the moon</h1>
+			<h2>¡¿Quién dijo que el límite es el cielo?!</h2>
+		</>
+	);
+}
+
+export default Home;
